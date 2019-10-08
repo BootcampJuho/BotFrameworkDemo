a99cb8061d58245c99844fc24fff2f54c89976c0
Juho <juho.frantti@gmail.com>
2019-10-08 23:06:38 +0300

    Kolmas kommit

---
+++ BottiDemo/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+function ChatNappi() {
+    var x = document.getElementById("Palkki");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
